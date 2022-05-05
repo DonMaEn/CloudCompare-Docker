81f2a2e04dbc6943d64aa18f62b5aed6fa0cc9d0
Maze-England Donald (dsm2588) <dsm2588@CSR-A55418.csr.utexas.edu>
2022-05-05 15:06:13 -0500

    Trying to fix docker image tagging issue

--- build.sh
+++ build.sh
@@ -3,15 +3,12 @@
 # Exit on error
 set -e
 
-release=$(git describe --tags --long)
-
 # Get dockerfile directory from script location
 dockerdir=$(realpath $(dirname ${0}))
 
 # Tag the docker with 'latest'
-tag=donaldmaen/cloudcompare:${release}
+tag=donaldmaen/cloudcompare:latest
 echo Building ${tag}
 docker build \
     --tag="${tag}" \
     ${dockerdir}
-docker tag donaldmaen/cloudcompare:${release} donaldmaen/cloudcompare:latest
