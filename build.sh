9a108f420ab90c1d3ef2f080ac53ff2e3135aa6f
Maze-England Donald (dsm2588) <dsm2588@CSR-A55418.csr.utexas.edu>
2022-05-05 14:18:14 -0500

    Changed build.sh to tag the docker with the git repo tag as well as latest

--- build.sh
+++ build.sh
@@ -3,12 +3,15 @@
 # Exit on error
 set -e
 
+release=$(git describe --tags --long)
+
 # Get dockerfile directory from script location
 dockerdir=$(realpath $(dirname ${0}))
 
 # Tag the docker with 'latest'
-tag=ut/cloudcompare:latest
+tag=donaldmaen/cloudcompare:${release}
 echo Building ${tag}
 docker build \
     --tag="${tag}" \
     ${dockerdir}
+docker tag donaldmaen/cloudcompare:${release} donaldmaen/cloudcompare:latest
